52497d0cfda95a05ba25af4567c5d4e4f4aa3ca9
Carmen Wolvius <carmen.wolvius@gmail.com>
2021-12-14 22:01:35 +0100

    script for performance testing

---
+++ credit score case study/scripts/Performance testing.R
@@ -0,0 +1,11 @@
+##################################
+## ADC Case Study - Credit Scoring
+## Performance Testing
+## 16-12-2021
+## Carmen Wolvius
+## carmen.wolvius@gmail.com
+##################################
+
+#### Load Libraries
+library(here)
+library(tidyverse)
