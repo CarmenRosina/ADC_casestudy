fe4b216c6ffb51b3906012b41297c9403c21c95c
Carmen Wolvius <carmen.wolvius@gmail.com>
2021-12-15 21:24:39 +0100

    model fit

--- credit score case study/scripts/Performance testing.R
+++ credit score case study/scripts/Performance testing.R
@@ -9,3 +9,33 @@
 #### Load Libraries
 library(here)
 library(tidyverse)
+
+## Load cleaned train data
+train_data_cleaned <- read.csv2(here("data/processed/","training_set_cleaned.csv"))
+
+train <- train_data_cleaned[1:800,]
+test <- train_data_cleaned[801:889,]
+
+## Fit model
+model <- glm(Survived ~.,
+             family=binomial(link='logit'),
+             data=train)
+
+summary(model)
+
+# ANOVA: The difference between the null deviance and the residual deviance shows how our model is doing against the null model 
+anova(model, test="Chisq")
+
+## Test model
+fitted.results <- predict(model, 
+                          newdata=test %>% 
+                            select(Pclass, Sex, Age, SibSp, Parch, Fare, Embarked), 
+                          type='response')
+
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+
+results <- data.frame(predicted = fitted.results, 
+                      actual = test$Survived)
+results <- results %>%
+  mutate(score = ifelse(predicted == actual, 1, 0))
+
